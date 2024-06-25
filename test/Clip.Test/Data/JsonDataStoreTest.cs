bfab85f6df2048b31cff7cf6081adea779e537a3
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-06-25 20:27:37 -0300

    Creating some new tests

--- test/Clip.Test/Data/JsonDataStoreTest.cs
+++ test/Clip.Test/Data/JsonDataStoreTest.cs
@@ -1,4 +1,3 @@
-using System.Text.Json;
 using Clip.Data;
 using Clip.Data.Configuration;
 using Clip.Test.TestInfrastructure;
@@ -35,10 +34,15 @@ public class JsonDataStoreTest
     }
 
     [Test]
-    public async Task AddNewObjectAsync()
+    public async Task UserDataDoesntExistIfNotPreviouslyStored()
     {
         var userData = await _userDataDataStore.RetrieveDataAsync(_cancellationToken);
         Assert.That(userData is null);
+    }
+    
+    [Test]
+    public async Task AddOneObjectAsync()
+    {
         var testUserId = Guid.NewGuid().ToString(); 
         var user = new User()
         {
@@ -47,12 +51,65 @@ public class JsonDataStoreTest
             Email = "test.user@email.com",
             CreationTime = DateTime.UtcNow
         };
-        userData = new UserData();
+        var userData = new UserData();
         userData.Users.Add(user);
         await _userDataDataStore.StoreDataAsync(userData, _cancellationToken);
         userData = await _userDataDataStore.RetrieveDataAsync(_cancellationToken);
         Assert.That(userData is not null);
         Assert.That(userData.Users.Count() == 1);
         Assert.That(userData.Users.All(x => x.Id == testUserId));
+    }
+    
+    [Test]
+    public async Task AddMultipleObjectsAsync()
+    {
+        var users = CreateUsers(10);
+        var userData = new UserData
+        {
+            Users = users
+        };
+        await _userDataDataStore.StoreDataAsync(userData, _cancellationToken);
+        userData = await _userDataDataStore.RetrieveDataAsync(_cancellationToken);
+        Assert.That(userData is not null);
+        Assert.That(userData.Users.Count() == 10);
+        for (var i = 0; i < 10; i++)
+            Assert.That(userData.Users.Where(user => user.Id == i.ToString()).Count() == 1);
+    }
+
+    [Test]
+    public async Task RemoveOneObjectAsync()
+    {
+        var users = CreateUsers(10);
+        var userData = new UserData
+        {
+            Users = users
+        };
+        await _userDataDataStore.StoreDataAsync(userData, _cancellationToken);
+        userData = await _userDataDataStore.RetrieveDataAsync(_cancellationToken);
+        userData.Users.RemoveAt(0);
+        await _userDataDataStore.StoreDataAsync(userData, _cancellationToken);
+        userData = await _userDataDataStore.RetrieveDataAsync(_cancellationToken);
+        Assert.That(userData is not null);
+        Assert.That(userData.Users.Count() == 9);
+        for (var i = 1; i < 10; i++)
+            Assert.That(userData.Users.Where(user => user.Id == i.ToString()).Count() == 1);
+        Assert.That(userData.Users.Where(user => user.Id == "0").Count() == 0);
+    }
+
+    public List<User> CreateUsers(int amount)
+    {
+        var users = new List<User>();
+        for (var i = 0; i < amount; i++)
+        {
+            var user = new User()
+            {
+                Id = i.ToString(),
+                Name = $"User {i}",
+                Email = $"user{i}@email.com",
+                CreationTime = DateTime.UtcNow
+            };
+            users.Add(user);
+        }
+        return users;
     }
 }
